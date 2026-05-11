52212e76748eb86d3da24aa12aa8fce7d6ca62c1
Fatema <fatematz486@gmail.com>
2026-05-11 18:00:50 +0600

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Banner from "@/components/Banner";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,9 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <Navbar/>
+        <Banner/>
         {children}
+        <Footer/>
         </body>
     </html>
   );
