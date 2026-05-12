756c98f9e326ee09731fe09be17ac3e2c4fbefaf
Fatema <fatematz486@gmail.com>
2026-05-12 10:54:58 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 const Navbar = () => {
     return (
         <div className="  py-4 absolute  w-full" >
-            <div className="flex justify-between items-center  bg-white px-4 max-w-[1700px] mx-auto w-full ">
+            <div className="flex justify-between items-center  bg-white px-4 max-w-[1400px] w-full mx-auto ">
                 <div className="">
                     <ul className="flex gap-4">
                         <li> <Link href='/'>Home</Link></li>
                         <li> <Link href='/destinations'>Destinations</Link></li>
                         <li> <Link href='/bookings'>My Bookings</Link> </li>
-                        <li> <Link href='/admin'>Admin</Link></li>
+                        <li> <Link href='/addDestination'>Add Destination</Link></li>
                     </ul>
                 </div>
                 <div className="">
